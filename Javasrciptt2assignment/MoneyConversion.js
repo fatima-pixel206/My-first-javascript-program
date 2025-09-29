45f621e077b3306a334aa55477aae309459175c1
Fatima Arain <fatimaarain67@gmail.com>
2025-09-29 08:03:40 -0700

    Update MoneyConversion.js

--- Javasrciptt2assignment/MoneyConversion.js
+++ Javasrciptt2assignment/MoneyConversion.js
@@ -2,19 +2,26 @@
 
  let Us=282.46;
 let saudiRiyal=75.03;
-let Usd=+prompt(" Enter  US amount you want to convert in PKR");
-let UStoPkr=Us*Usd;
-let SaudiR=+prompt(" Enter your   amount in Saudi currency to convert into PKR: ");
-let saudiRiyalToPkr=saudiRiyal*SaudiR;
-console.log(" your amount in us dollar is "+Usd+" \n your Pakistani amount is "+UStoPkr);
-console.log(" your saudi amount is "+SaudiR+"\n your Pakistani amount is "+saudiRiyalToPkr);
-let totalPKR=UStoPkr+saudiRiyalToPkr;
+
+let UsDollar=+prompt(" Enter  US amount you want to convert in PKR");
+let UstoPkR=Us*UsDollar;
+
+let SaudiRiyal=+prompt(" Enter your   amount in Saudi currency to convert into PKR: ");
+let saudiRiyaltoPkR=saudiRiyal*SaudiRiyal;
+
+console.log(" your amount in us dollar is "+UsDollar+" \n your Pakistani amount is "+USstoPkR);
+console.log(" your saudi amount is "+SaudiR+"\n your Pakistani amount is "+saudiRiyaltoPkR);
+let totalPKR=UStoPkR+saudiRiyaltoPkR;
+
 console.log(" you have  total amount in Pakistani PKR is:" +totalPKR);
 // conversion PKR in US dollar and Saudi riyal
-let exchangeAmountINUs=+prompt(" enter your Pakistani amount to change in us dollars: ");
-let PkRtoUS=exchangeAmountINUs/Us;
-let exchangeAmountInSAr=+prompt(" enter your Pakistani amount to change in Saudi Riyal : ");
-let PkRtoSAR=exchangeAmountInSAr/saudiRiyal;
-console.log(" your Pakistani amount is "+exchangeAmountINUs+"\n you have "+PkRtoUS+"  Us Dollars");
+
+
+let exchangeAmountinUS=+prompt(" enter your Pakistani amount to change in us dollars: ");
+let PkRtoUS=exchangeAmountinUS/US;
+let exchangeAmountinSAR=+prompt(" enter your Pakistani amount to change in Saudi Riyal : ");
+let PkRtoSAR=exchangeAmountinSAR/saudiRiyal;
+console.log(" your Pakistani amount is "+exchangeAmountinUS+"\n you have "+PkRtoUS+"  Us Dollars");
 console.log(" your Pakistani amount is "+exchangeAmountInSAr+"\n you have "+PkRtoSAR+" Saudi Riyal");
+
 
